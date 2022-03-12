52c89cadec0f6cf99720954ac6eab3491dad0b29
S1nd1n <krez2010@gmail.com>
2022-03-12 14:40:27 +0300

    Практика с воркерами. !!!Воркер не считает - разобраться!

---
+++ worker.js
@@ -0,0 +1,8 @@
+//обработчик данных самого воркера
+let onmessage = ev => {
+    console.log('пришли данные', this);
+    if( !isNaN(ev.data.a) && !isNaN(ev.data.b)){
+        postMessage(ev.data.a + ev.data.b);
+    }
+}
+// console.log(this)
